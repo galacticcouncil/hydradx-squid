605a2fcc2d0864c8d8d114c772e0edee6bf349ec
green-jay <greenee@email.cz>
2023-05-12 14:20:00 +0200

    gcp

--- src/processor.ts
+++ src/processor.ts
@@ -7,7 +7,21 @@ import { OmnipoolAsset } from "./model"
 import { OmnipoolAssetsStorage } from "./types/storage"
 import { AssetState } from "./types/v115"
 import dotenv from 'dotenv';
+import http from 'http';
 dotenv.config();
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 const archive = process.env.ARCHIVE || lookupArchive('hydradx', { release: 'FireSquid' });
 const chain = process.env.CHAIN || 'wss://rpc.hydradx.cloud';
